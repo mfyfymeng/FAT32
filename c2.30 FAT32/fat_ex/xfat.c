dfd9b43a0143e71290ab21446862bbd35cf0a146
mfyfymeng <mfyfymeng@163.com>
2020-03-03 14:10:37 +0800

    first commit

---
+++ c2.30 FAT32/fat_ex/xfat.c
@@ -0,0 +1,104 @@
+#include <stdlib.h>
+#include "xfat.h"
+
+extern u8_t temp_buffer[512];
+
+#define xfat_get_disk(xfat)    ((xfat)->disk_part->disk)
+
+static xfat_err_t parse_fat_header(xfat_t* xfat, dbr_t* dbr) {
+	xdisk_part_t* xdisk_part = xfat->disk_part;
+
+	xfat->root_cluster = dbr->fat32.BPB_RootClus;
+	xfat->fat_tbl_sectors = dbr->fat32.BPB_FATSz32;
+
+	if (dbr->fat32.BPB_ExtFlags & (1 << 7)) {
+		u32_t table = dbr->fat32.BPB_ExtFlags & 0xF;
+		xfat->fat_start_sector = dbr->bpb.BPB_RsvdSecCnt + xdisk_part->start_sector + table * xfat->fat_tbl_sectors;
+		xfat->fat_tbl_nr = 1;
+	}
+	else {
+		xfat->fat_start_sector = dbr->bpb.BPB_RsvdSecCnt + xdisk_part->start_sector;
+		xfat->fat_tbl_nr = dbr->bpb.BPB_NumFATs;
+	}
+
+	xfat->sec_per_cluster = dbr->bpb.BPB_SecPerClus;
+	xfat->total_sectors = dbr->bpb.BPB_TotSec32;
+	xfat->cluster_byte_size = xfat->sec_per_cluster * dbr->bpb.BPB_BytsPerSec;
+
+	return FS_ERR_OK;
+}
+
+xfat_err_t xfat_open(xfat_t* xfat, xdisk_part_t* xdisk_part) {
+	dbr_t* dbr = (dbr_t*)temp_buffer;
+	xdisk_t* xdisk = xdisk_part->disk;
+	xfat->disk_part = xdisk_part;
+
+	int err = xdisk_read_sector(xdisk, (u8_t*)dbr, xdisk_part->start_sector, 1);
+	if (err) return err;
+
+	err = parse_fat_header(xfat, dbr);
+	if (err) return err;
+
+	xfat->fat_buffer = (u8_t*)malloc(xfat->fat_tbl_sectors * xdisk->sector_size);
+	err = xdisk_read_sector(xdisk, (u8_t*)xfat->fat_buffer, xfat->fat_start_sector, xfat->fat_tbl_sectors);
+	if (err < 0) return err;
+
+	return FS_ERR_OK;
+}
+
+u32_t cluster_first_sector(xfat_t* xfat, u32_t cluster_no) {
+	u32_t data_start_sector = xfat->fat_start_sector + xfat->fat_tbl_sectors * xfat->fat_tbl_nr;
+	return data_start_sector + (cluster_no - 2) * xfat->sec_per_cluster;
+}
+
+xfat_err_t read_cluster(xfat_t* xfat, u8_t* buffer, u32_t cluster, u32_t count) {
+	xfat_err_t err = 0;
+	u8_t* curr_buffer = buffer;
+	u32_t curr_sector = cluster_first_sector(xfat, cluster);
+
+	for (u32_t i = 0; i < count; i++) {
+		err = xdisk_read_sector(xfat_get_disk(xfat), curr_buffer, curr_sector, xfat->sec_per_cluster);
+		if (err < 0) return err;
+
+		curr_buffer += xfat->cluster_byte_size;
+		curr_sector += xfat->sec_per_cluster;
+	}
+
+	return FS_ERR_OK;
+}
+
+int is_cluster_valid(u32_t cluster) {
+	cluster &= 0x0FFFFFFF;
+	return (cluster < 0x0FFFFFF0) && (cluster >= 2);
+}
+
+xfat_err_t get_next_cluster(xfat_t* xfat, u32_t curr_cluster_no, u32_t* next_cluster) {
+	if (is_cluster_valid(curr_cluster_no)) {
+		cluster32_t* cluster32_buf = (cluster32_t*)xfat->fat_buffer;
+		*next_cluster = cluster32_buf[curr_cluster_no].s.next;
+	}
+	else
+		*next_cluster = CLUSTER_INVALID;
+
+	return FS_ERR_OK;
+}
+
+static xfat_err_t open_sub_file(xfat_t* xfat, u32_t dir_cluster, xfile_t* file, const char* path) {
+	file->size = 0;
+	file->type = FAT_DIR;
+	file->start_cluster = dir_cluster;
+	file->curr_cluster = dir_cluster;
+	file->xfat = xfat;
+	file->pos = 0;
+	file->err = FS_ERR_OK;
+	file->attr = 0;
+	return FS_ERR_OK;
+}
+
+xfat_err_t xfile_open(xfat_t* xfat, xfile_t* file, const char* path) {
+	return open_sub_file(xfat, xfat->root_cluster, file, path);
+}
+
+xfat_err_t xfile_close(xfile_t* file) {
+	return FS_ERR_OK;
+}
---
+++ c2.30 FAT32/fat_ex/xfat.h
@@ -0,0 +1,135 @@
+#ifndef XFAT_H
+#define XFAT_H
+
+#include "xtypes.h"
+#include "xdisk.h"
+
+#pragma pack(1)
+
+typedef struct _bpb_t {
+	u8_t  BS_jmpBoot[3];
+	u8_t  BS_OEMName[8];
+	u16_t BPB_BytsPerSec;
+	u8_t  BPB_SecPerClus;
+	u16_t BPB_RsvdSecCnt;
+	u8_t  BPB_NumFATs;
+	u16_t BPB_RootEntCnt;
+	u16_t BPB_TotSec16;
+	u8_t  BPB_Media;
+	u16_t BPB_FATSz16;
+	u16_t BPB_SecPerTrk;
+	u16_t BPB_NumHeads;
+	u32_t BPB_HiddSec;
+	u32_t BPB_TotSec32;
+}bpb_t;
+
+typedef struct _fat32_hdr_t {
+	u32_t BPB_FATSz32;
+	u16_t BPB_ExtFlags;
+	u16_t BPB_FSVer;
+	u32_t BPB_RootClus;
+	u16_t BPB_FsInfo;
+	u16_t BPB_BkBootSec;
+	u8_t  BPB_Reserved[12];
+	u8_t  BS_DrvNum;
+	u8_t  BS_Resvered1;
+	u8_t  BS_BootSig;
+	u32_t BS_VolID;
+	u8_t  BS_VolLab[11];
+	u8_t  BS_FileSysType[8];
+}fat32_hdr_t;
+
+typedef struct _dbr_t {
+	bpb_t bpb;
+	fat32_hdr_t fat32;
+}dbr_t;
+
+#define CLUSTER_INVALID                 0xFFFFFFF
+
+#define DIRITEM_NAME_FREE               0xE5
+#define DIRITEM_NAME_END                0x00
+
+#define DIRITEM_ATTR_READ_ONLY          0x01
+#define DIRITEM_ATTR_HIDDEN             0x02
+#define DIRITEM_ATTR_SYSTEM             0x04
+#define DIRITEM_ATTR_VOLUME_ID          0x08
+#define DIRITEM_ATTR_DIRECTORY          0x10
+#define DIRITEM_ATTR_ARCHIVE            0x20
+#define DIRITEM_ATTR_LONG_NAME          0x0F
+
+typedef struct _diritem_date_t {
+	u16_t day : 5;
+	u16_t month : 4;
+	u16_t year_from_1980 : 7;
+}diritem_date_t;
+
+typedef struct _diritem_time_t {
+	u16_t second_2 : 5;
+	u16_t minute : 6;
+	u16_t hour : 5;
+}diritem_time_t;
+
+typedef struct _diritem_t {
+	u8_t DIR_Name[8];
+	u8_t DIR_ExtName[3];
+	u8_t DIR_Attr;
+	u8_t DIR_NTRes;
+	u8_t DIR_CrtTimeTeenth;
+	diritem_time_t DIR_CrtTime;
+	diritem_date_t DIR_CrtDate;
+	diritem_date_t DIR_LastAccDate;
+	u16_t DIR_FstClusHI;
+	diritem_time_t DIR_WrtTime;
+	diritem_date_t DIR_WrtDate;
+	u16_t DIR_FstClusL0;
+	u32_t DIR_FileSize;
+}diritem_t;
+
+typedef union _cluster32_t {
+	struct {
+		u32_t next : 28;
+		u32_t reserved : 4;
+	}s;
+	u32_t v;
+}cluster32_t;
+
+#pragma pack()
+
+typedef struct _xfat_t {
+	u32_t fat_start_sector;
+	u32_t fat_tbl_nr;
+	u32_t fat_tbl_sectors;
+	u32_t sec_per_cluster;
+	u32_t root_cluster;
+	u32_t cluster_byte_size;
+	u32_t total_sectors;
+	u8_t* fat_buffer;
+	xdisk_part_t* disk_part;
+}xfat_t;
+
+typedef enum _xfile_type_t {
+	FAT_DIR,
+	FAT_FILE,
+	FAT_VOL,
+}xfile_type_t;
+
+typedef struct _xfile_t {
+	xfat_t* xfat;
+	u32_t size;
+	u16_t attr;
+	xfile_type_t type;
+	u32_t pos;
+	xfat_err_t err;
+	u32_t start_cluster;
+	u32_t curr_cluster;
+}xfile_t;
+
+int is_cluster_valid(u32_t cluster);
+xfat_err_t get_next_cluster(xfat_t* xfat, u32_t curr_cluster_no, u32_t* next_cluster);
+xfat_err_t xfat_open(xfat_t* xfat, xdisk_part_t* xdisk_part);
+xfat_err_t read_cluster(xfat_t* xfat, u8_t* buffer, u32_t cluster, u32_t count);
+
+xfat_err_t xfile_open(xfat_t* xfat, xfile_t* file, const char* path);
+xfat_err_t xfile_close(xfile_t* file);
+
+#endif // !XFAT_H
